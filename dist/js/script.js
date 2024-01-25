82104dd10444fa3bcfde8d8597ba65930840aaeb
rinklepoonia <rinklepoonia889@gmail.com>
2024-01-25 09:39:28 +0530

    shadow,nav etc

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,22 @@
 function openNav() {
     document.querySelector(".menuList").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
+    document.body.classList.toggle("overflow-hidden");
     document.querySelector(".menuIcon").classList.toggle("cross")
 }
+
+
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+})
